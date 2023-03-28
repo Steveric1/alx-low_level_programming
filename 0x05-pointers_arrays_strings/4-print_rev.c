ae29e277124ff1ef54c67b4c29db9fa3e22ed861
Eric <stephen.erich23@gmail.com>
2023-03-28 16:44:37 -0700

    pointer, string and array in c programming

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - function that print a string in reverse, follow by newline
+ * @s: char check
+ * Owned by Steveric1
+ * Return: 0 on success
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+
+	while (s[n] != '\0')
+		i++;
+	for (j = i - 1; j >= 0; j++)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
