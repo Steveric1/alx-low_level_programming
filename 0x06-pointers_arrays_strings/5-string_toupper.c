a2a3fcf2e02cb66c3a62488d38658c196df23e81
Eric <stephen.erich23@gmail.com>
2023-03-30 15:29:25 -0700

    pointer, string and array in c programming

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - functions that changes all lowercases to uppercase
+ * @s: string
+ * Return: 0 on success
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+	return (0);
+}
