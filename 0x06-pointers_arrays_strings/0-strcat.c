2fd6b0f2da935890fbaf332c69a5dfb147d4dbd2
Eric <stephen.erich23@gmail.com>
2023-03-30 11:57:39 -0700

    pointer, string and array in c programming

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+	while (*p != '\0')
+		p++;
+	while (*src != '\0')
+		*p++ = *src++;
+	*p = '\0';
+	return dest;
+}
