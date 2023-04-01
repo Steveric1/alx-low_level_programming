15d869ebf69e18440f995bcee5167c6905dea5bd
Eric <stephen.erich23@gmail.com>
2023-03-31 17:59:41 -0700

    pointer, string and array in c programming

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: number to be printed
+ * Return: 0 on success
+ */
+void print_number(int n)
+{
+	unsigned int y = 0;
+
+	if (n < 0)
+	{
+		y = -n;
+	}
+	y = n;
+
+	if (y / 10)
+		print_number(y / 10);
+	_putchar(y % 10 + '0');
+}
