3aec1ca67c3ee62e7a0bc9236b53990c8d7e81a8
Eric <stephen.erich23@gmail.com>
2023-04-09 10:25:16 -0700

    argc task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of argument passed into it
+ * @argc: argument count
+ * @argv: argments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
