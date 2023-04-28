423b51e0a680289e8d27f2470a6e4e46b7995a0b
Eric <stephen.erich243@gmail.com>
2023-04-28 14:32:12 -0700

    singly linked list task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
+#include "lists.h"
 
 /**
- * free_list - free memory allocated for the list
+ * free_list - Realease the memory allocated for a list
  *
- * @head: a pointer to the first node
+ * @head: A pointer to the first node of the list to free
  */
-
 void free_list(list_t *head)
 {
 	if (head)
