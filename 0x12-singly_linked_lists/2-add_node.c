cb5398e509dc441286ddd8cfd0d6d831eab00723
Eric <stephen.erich243@gmail.com>
2023-04-28 10:37:08 -0700

    singly linked list task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - function that returns the length of a string.
+ * @s : s is a character
+ * Return: value is i
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * add_node - add a new node at beginning of a list_t list.
  * @head: head of a list_t list.
  * @str: value to insert into element.
@@ -13,21 +28,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	unsigned int len = 0;
-
 	new_node = malloc(sizeof(list_t));
-	
+
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
-
-	while (str[len])
-	{
-		len++;
-	}
-
-	new_node->len = len;
-	new_node = *head;
+	new_node->len = _strlen(str);
+	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
