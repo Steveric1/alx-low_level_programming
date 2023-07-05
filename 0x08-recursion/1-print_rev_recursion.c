dee3a9205c9378ac57dcfcf40a8583ea6484ad84
Eric <stephen.erich243@gmail.com>
2023-07-05 10:19:34 -0700

    recursion in c programming

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
-#define "main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
  * _print_rev_recursion - print string in reverse
@@ -6,10 +7,12 @@
  * Return: 0 on success
  */
 
+void _print_rev_recursion(char *s);
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
 	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	 putchar(*s);
 }
