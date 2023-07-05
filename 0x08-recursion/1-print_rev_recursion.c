c79aba9823eaccc7953328f254ccd4882baa20f8
Eric <stephen.erich243@gmail.com>
2023-07-05 07:04:10 -0700

    recursion in c programming

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#define "main.h"
+
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: string
+ * Return: 0 on success
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
