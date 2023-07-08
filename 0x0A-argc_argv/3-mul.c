77253ad989b4956896b4f3b67276e62ce776eacc
Eric <stephen.erich243@gmail.com>
2023-07-07 18:00:53 -0700

    learning argc and argv in c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of argument passed
+ * @argc: size of array
+ * @argv: array
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	printf("%d\n", x * y);
+
+	return (0);
+}
