479ebc225876ae39240eca7140522f149f261ccb
Eric <stephen.erich243@gmail.com>
2023-07-10 18:18:21 -0700

    solving malloc task

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free the two dimensional array
+ * @grid: grid function
+ * @height: the row
+ *
+ * return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
