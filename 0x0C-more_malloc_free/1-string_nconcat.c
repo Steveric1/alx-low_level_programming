aa13ba8717b2306137528c97ce6931aab3c073cb
Eric <stephen.erich243@gmail.com>
2023-07-13 06:37:40 -0700

    malloc free task solution

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,12 +18,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s3;
 
 	if (s1 == NULL)
-		s1 = " ";
+		len1 = strlen(s1);
 	if (s2 == NULL)
-		s2 = " ";
-
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+		len2 = strlen(s2);
 
 	if (n >= len2)
 		n = len2;
