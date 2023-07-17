7bfe7c472eaa83330f4084370a6ebbc542e543f2
Eric <stephen.erich243@gmail.com>
2023-07-17 16:34:31 -0700

    solving typedef task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include "string.h"
+#include <stdlib.h>
+
+/*
+ * init_dog - initialize a varible of type struct dog
+ * @d: pointer variable to struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * return: 0 on success
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	strcpy(d->name, name);
+	d->age = age;
+	strcpy(d->owner, owner);
+}
