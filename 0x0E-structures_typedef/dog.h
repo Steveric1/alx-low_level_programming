2449661c1370c99f80e500fdfc7cdbfc8547cd20
Eric <stephen.erich243@gmail.com>
2023-07-17 17:20:30 -0700

    solving typedef task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 
 #include <stdio.h>
 
@@ -19,4 +19,6 @@ struct dog
 	char *owner;
 };
 
-#endif /*DOG_H*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif /*DOG*/
