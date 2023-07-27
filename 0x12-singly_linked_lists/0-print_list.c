10ca937cc25f1d3918aaf5425a3cced8828ad8cd
Eric <stephen.erich243@gmail.com>
2023-07-27 16:42:28 -0700

    solving singly linked list task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,8 +9,7 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *ptr = malloc(sizeof(list_t));
-	ptr = h;
+	const list_h *ptr = h;
 	size_t count_node = 0;
 
 	while (ptr != NULL)
@@ -18,7 +17,7 @@ size_t print_list(const list_t *h)
 		if (ptr->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		ptr = ptr->next;
 		count_node++;
 	}
