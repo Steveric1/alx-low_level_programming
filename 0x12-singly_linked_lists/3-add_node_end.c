51e5e0dfc93fa07baffc1c66ab867b3432f5108b
Eric <stephen.erich243@gmail.com>
2023-07-28 02:08:30 -0700

    solving singly linked list task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,45 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - add a new node at the end
- * @head: head
- * @str: str to duplicate
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
  *
- * Return: 0 on success
+ * Return: length of string
  */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
 
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node to the end of linked list
+ * @head: double pointer to a linked list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *s *ptr;
+	list_t *new, *tmp;
 
 	if (str == NULL)
 		return (NULL);
-
-	s = malloc(sizeof(list_t));
-
-	if (s == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-
-	s->str = strdup(str);
-	if(s->str == NULL)
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
-		free(s);
+		free(new);
 		return (NULL);
 	}
-
-	s->len = strlen(str);
-	s->next = NULL;
-
+	new->len = _strlen(new->str);
+	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = s;
-		return (*head);
+		*head = new;
+		return (new);
 	}
-
-	ptr = *head;
-	while (ptr->next != NULL)
-		ptr = ptr->next;
-	ptr->next = s;
-
-	return (s);
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
 }
