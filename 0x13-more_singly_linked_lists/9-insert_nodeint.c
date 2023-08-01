ddcea26e4f103f5064795ae7aee174e979a3ac7f
Eric <stephen.erich243@gmail.com>
2023-08-01 13:20:01 -0700

    most singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,8 +23,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		while (i < idx - 1 && ptr != NULL)
 		{
-			ptr = ptr->next;
 			i++;
+			ptr = ptr->next;
 		}
 
 		if (ptr == NULL)
