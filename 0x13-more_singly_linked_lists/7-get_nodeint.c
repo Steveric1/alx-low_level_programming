1ec273cc4167e0119de8e68e057bb1ffb3c85998
Eric <stephen.erich243@gmail.com>
2023-08-01 10:24:21 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	int count = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (head != NULL)
 	{
 		if (count == index)
@@ -19,6 +22,4 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 		count++;
 	}
-
-	return NULL;
 }
