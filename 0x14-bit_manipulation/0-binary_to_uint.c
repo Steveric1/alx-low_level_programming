7f4a39cfe568496ec5b6f5315f70f92f2be6dc1e
Eric <stephen.erich243@gmail.com>
2023-08-03 12:36:34 -0700

    solving bit manipulation tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,46 +1,29 @@
 #include "main.h"
+
 /**
- * _power - calculate (base and power)
- * @base: base of the exponet
- * @pow: power of the exponet
- * Return: value of base and power
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return:converted number, or 0
  */
-unsigned long int _power(unsigned int base, unsigned int pow)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned long int num;
-	unsigned int i;
+	int i;
+	unsigned int j;
 
-	num = 1;
-	for (i = 1; i <= pow; i++)
-		num *= base;
-	return (num);
-}
-/**
- * print_binary - prints the binary representation of a number
- * @n: num of prented
- * Return: void
- */
-void print_binary(unsigned long int n)
-{
-	unsigned long int dev, result;
-	char flag;
-
-	flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
-
-	while (dev != 0)
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		result = n & dev;
-		if (result == dev)
-		{
-			flag = 1;
-			_putchar('1');
-
-		}
-		else if (flag == 1 || dev == 1)
-		{
-			_putchar('0');
-		}
-		dev >>= 1;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
