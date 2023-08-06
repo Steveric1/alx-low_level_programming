ee98a78fe7933af94358718fb07eebf4488a3480
Eric <stephen.erich243@gmail.com>
2023-08-06 11:34:41 -0700

    solving bit mainipulation task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,47 +1,32 @@
 #include "main.h"
-/**
- * _power - calculate (base and power)
- * @base: base of the exponet
- * @pow: power of the exponet
- * Return: value of base and power
- */
-unsigned long int _power(unsigned int base, unsigned int pow)
-{
-	unsigned long int num;
-	unsigned int i;
 
-	num = 1;
-	for (i = 1; i <= pow; i++)
-		num *= base;
-	return (num);
-}
 /**
  * print_binary - prints the binary representation of a number
  * @n: num of prented
  * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int dev, result;
-	char flag;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int flag = 0;
+	unsigned long int result;
 
-	flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
-
-	while (dev != 0)
+	while (mask != 0)
 	{
-		result = n & dev;
-		if (result == dev)
+		result = n & mask;
+		if (result == mask)
 		{
 			flag = 1;
 			_putchar('1');
-
 		}
-		else if (flag == 1 || dev == 1)
+		else
 		{
-			_putchar('0');
+			if (flag == 1 || mask == 1)
+				_putchar('0');
 		}
-		dev >>= 1;
+
+		mask >>= 1;
 	}
 }
 
