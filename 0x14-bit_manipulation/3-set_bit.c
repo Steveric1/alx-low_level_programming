26156d93fea37bcbf0d71dc9d73dc5569c182201
Eric <stephen.erich243@gmail.com>
2023-08-06 19:32:32 -0700

    solving bit manipulation task

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,17 +3,17 @@
 /**
  * set_bit - the value of a bit to 1 at a given index.
  * @index: starting from 0 of the bit you want to set
- * @n: pointer number of i
+ * @n: pointer number of mask
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i;
+	unsigned long int mask;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	i = 1 << index;
-	*n = *n | i;
+	mask = 1UL << index;
+	*n |= mask;
 
 	return (1);
 }
