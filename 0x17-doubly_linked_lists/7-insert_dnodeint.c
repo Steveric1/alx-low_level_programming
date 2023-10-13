328203e923c5013266b46f7883772e3289e4ed52
Eric <stephen.erich243@gmail.com>
2023-10-12 17:37:29 -0700

    doubly linkedlist

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		for (i = 0; i < idx - 1 && ptr != NULL; i++)
 		{
 			ptr = ptr->next;
-			
 			if (ptr == NULL)
 				return (NULL);
 		}
