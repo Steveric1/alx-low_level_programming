96081bdfdf44b51d3ce01a8596cebc43df0c1738
Eric <stephen.erich243@gmail.com>
2023-10-20 09:24:03 -0700

    Shared library

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - replace buffer
+ * @s: pointer to buffer
+ * @b: second buffer
+ * @n: amount of changed buffers
+ * Return: temp
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *temp = s;
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (temp);
+}
