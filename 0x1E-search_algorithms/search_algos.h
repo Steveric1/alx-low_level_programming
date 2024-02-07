46be81abda978649c5e37ef058f431ed1951310f
Eric <stephen.erich243@gmail.com>
2024-02-07 12:52:28 +0100

    searching algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,7 +8,4 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 
-/*Search Algorithm Helper*/
-size_t min(size_t a, size_t b);
-
 #endif /*SEARCH_ALGOS_H*/
